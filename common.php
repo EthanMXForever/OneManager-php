00a7d62831f178c6557d594e77c28b22d16a2334
Luxiang Zhang <38901375+luxiangzh@users.noreply.github.com>
2023-10-01 19:56:28 -0700

    Delete &OriginDomain

--- common.php
+++ common.php
@@ -636,10 +636,10 @@ function proxy_replace_domain($url, $domainforproxy, &$header) {
     //return $aim . '/' . $uri;
     if (strpos($url, '?') > 0) $sp = '&';
     else $sp = '?';
-    $aim .= '/' . $uri . $sp . 'Origindomain=' . $domain;
+    $aim .= '/' . $uri;
     if ($drive->show_base_class() == 'Aliyundrive') $aim .= '&Aliyundrive';
     $header['Location'] = $aim;
-    return $aim . '/' . $uri . $sp . 'Origindomain=' . $domain;
+    return $aim . '/' . $uri;
 }
 
 function bchexdec($hex) {
